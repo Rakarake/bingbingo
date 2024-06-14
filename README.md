805c621011d3b03aaa02feb527a11bee7add2686
Rakarake <raka@rakarake.xyz>
2024-06-14 12:11:34 +0200

    Much CSS progress

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Shell stuff probably needed for yew
 nix-shell -p cargo trunk cargo-binutils gcc lld
+
+# Resources
+Grid Layouts (grids with explicit locations): https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout
+Drag and drop: https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
+
--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,20 @@ fn App() -> Html {
 
     html! {
         <>
+            <h1> {"The BING 🗿 bingo!"} </h1>
+              <div class="bingo-card">
+                <div class="bingo-item">{"Hot springs"}</div>
+                <div class="bingo-item">{"Truck kun"}</div>
+                <div class="bingo-item">{"3"}</div>
+                <div class="bingo-item">{4}</div>
+                <div class="bingo-item">{5}</div>
+                <div class="bingo-item">{6}</div>
+                <div class="bingo-item">{7}</div>
+                <div class="bingo-item">{8}</div>
+                <div class="bingo-item">{9}</div>
+              </div> 
             <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
+            <p class="amongus">{ *counter }</p>
         </>
     }
 }
