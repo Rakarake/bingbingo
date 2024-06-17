f3a1453d67dc4be2b4d8e724becf12f60f900040
Rakarake <raka@rakarake.xyz>
2024-06-17 18:03:50 +0200

    Crossing tiles

--- public/code.js
+++ public/code.js
@@ -1,17 +1,29 @@
 console.log("amongus 🤨")
 
-function dragstartHandler(ev) {
-  // Add the target element's id to the data transfer object
-  ev.dataTransfer.setData("text/plain", ev.target.id);
-  ev.dataTransfer.dropEffect = "move";
+// Crossing tiles
+var applyCross = function(element) {
+    if (!element.classList.contains("crossed")) {
+        element.classList.add("crossed");
+    } else {
+        element.classList.remove("crossed");
+    }
 }
-
-window.addEventListener("DOMContentLoaded", () => {
-  // Get the element by id
-  const element = document.getElementById("p1");
-  // Add the ondragstart event listener
-  element.addEventListener("dragstart", dragstartHandler);
-});
-
-
+var crossTile = function(event) {
+    // No context menu when right clicking
+    let element = event.srcElement;
+    if (element.classList.contains("bingo-item")) {
+        event.preventDefault();
+        applyCross(element);
+    } else {
+        console.log("GLLOOOOOBYYYEEEE");
+        console.log(element);
+        applyCross(element.parentElement);
+    }
+};
+var elements = document.getElementsByClassName("bingo-item");
+console.log(elements.length);
+for (var i = 0; i < elements.length; i++) {
+    console.log("gooa");
+    elements[i].addEventListener('contextmenu', crossTile, false);
+}
 
