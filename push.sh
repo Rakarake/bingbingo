cfcdbecebc416bc407c39bb452ce916b53eefaf0
Rakarake <raka@rakarake.xyz>
2024-07-15 16:19:32 +0200

    Yess

--- push.sh
+++ push.sh
@@ -1,4 +1,1 @@
 rsync -a public/ rakarake@raka.rakarake.xyz:/var/www/rakarake.xyz/public/bingbingo
-#ssh rakarake@raka.rakarake.xyz "rm -r /var/www/rakarake.xyz/public/bingbingo"
-#scp -r public/* rakarake@raka.rakarake.xyz:/var/www/rakarake.xyz/public/bingbingo
-#ssh rakarake@raka.rakarake.xyz "chmod 755 /var/www/rakarake.xyz/public/bingbingo"
